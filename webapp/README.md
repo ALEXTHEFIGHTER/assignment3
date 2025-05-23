c72908d7a2f990df18a22708be40344f8d322395
Alexander Laibacher <alexander.laibacher@edu.fh-joanneum.at>
2025-05-23 15:48:53 +0200

    trigger workflow

---
+++ webapp/README.md
@@ -0,0 +1,4 @@
+echo "Update" >> README.md
+git add .
+git commit -m "trigger GitHub workflow"
+git push
--- webapp/app.py
+++ webapp/app.py
@@ -5,30 +5,29 @@ import os
 app = Flask(__name__)
 
 def get_db_connection():
-    conn = psycopg2.connect(
+    return psycopg2.connect(
         host=os.environ['DB_HOST'],
         dbname=os.environ['DB_NAME'],
         user=os.environ['DB_USER'],
         password=os.environ['DB_PASSWORD']
     )
-    return conn
 
-@app.route('/add', methods=['POST'])
+@app.route("/add", methods=["POST"])
 def add():
-    data = request.json.get('data')
+    data = request.json.get("data")
     conn = get_db_connection()
     cur = conn.cursor()
-    cur.execute('INSERT INTO info (data) VALUES (%s);', (data,))
+    cur.execute("INSERT INTO info (data) VALUES (%s);", (data,))
     conn.commit()
     cur.close()
     conn.close()
-    return {'status': 'OK'}
+    return {"status": "ok"}
 
-@app.route('/get')
+@app.route("/get")
 def get():
     conn = get_db_connection()
     cur = conn.cursor()
-    cur.execute('SELECT * FROM info;')
+    cur.execute("SELECT * FROM info;")
     rows = cur.fetchall()
     cur.close()
     conn.close()
